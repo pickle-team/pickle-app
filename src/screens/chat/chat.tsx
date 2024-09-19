517131520ae744ec24c4cd1bce83d905acf0ddcd
SID12g <sid12g@naver.com>
2024-09-19 15:50:58 +0900

    fix: chat screen

--- src/screens/chat/chat.tsx
+++ src/screens/chat/chat.tsx
@@ -1,46 +1,65 @@
 import React from 'react';
 import {css} from '@emotion/native';
-import {View} from 'react-native';
+import {Platform, ScrollView, View} from 'react-native';
 import colors from '../../styles/color';
-import ScreenWrap from '../../components/screenWrap';
 import Header from '../../components/header';
-
 import ChatBox from '../../components/chatBox';
+import FloatingButton from '../../components/floatingButton';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export default function Chat() {
+  const insets = useSafeAreaInsets();
   return (
-    <ScreenWrap topColor={colors.background.black} padding={false}>
+    <View
+      style={css`
+        width: 100%;
+        height: 100%;
+        background-color: ${colors.background.black};
+      `}>
       <View
         style={css`
-          margin-left: 20px;
-        `}>
-        <Header title="Chat" />
-      </View>
-      <View
-        style={css`
-          height: 16px;
+          height: ${Platform.OS === 'ios' ? `${insets.top}px` : ''};
+          background-color: ${colors.background.black};
         `}
       />
-      <ChatBox
-        profileColor="#E58DFB"
-        name="Delicious Pickle"
-        content="Really?"
-      />
-      <ChatBox
-        profileColor="#E58DFB"
-        name="Delicious Pickle"
-        content="Really?"
-      />
-      <ChatBox
-        profileColor="#E58DFB"
-        name="Delicious Pickle"
-        content="Really?"
-      />
-      <ChatBox
-        profileColor="#E58DFB"
-        name="Delicious Pickle"
-        content="Really?"
-      />
-    </ScreenWrap>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={css`
+          padding-top: ${Platform.OS === 'ios' ? '' : '20px'};
+        `}>
+        <View
+          style={css`
+            margin-left: 20px;
+          `}>
+          <Header title="Chat" />
+        </View>
+        <View
+          style={css`
+            height: 16px;
+          `}
+        />
+        <ChatBox
+          profileColor="#E58DFB"
+          name="Delicious Pickle"
+          content="Really?"
+        />
+        <ChatBox
+          profileColor="#E58DFB"
+          name="Delicious Pickle"
+          content="Really?"
+        />
+        <ChatBox
+          profileColor="#E58DFB"
+          name="Delicious Pickle"
+          content="Really?"
+        />
+        <ChatBox
+          profileColor="#E58DFB"
+          name="Delicious Pickle"
+          content="Really?"
+        />
+      </ScrollView>
+      <FloatingButton />
+    </View>
   );
 }
