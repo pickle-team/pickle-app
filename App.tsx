432b5eb880ba3b1232bba157858e109fdc9f00c7
SID12g <sid12g@naver.com>
2024-09-20 08:36:49 +0900

    fix: bottom tab file

--- App.tsx
+++ App.tsx
@@ -1,136 +1,23 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
-import colors from './src/styles/color';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {initializeKakaoSDK} from '@react-native-kakao/core';
 import {KAKAO_NATIVE_APP_KEY} from '@env';
-import {RecoilRoot, useRecoilState} from 'recoil';
-import Setting from './src/screens/settings/setting';
-import Home from './src/screens/home/home';
-import Live from './src/screens/home/live';
-import Chat from './src/screens/chat/chat';
-import Profile from './src/components/profile';
+import {RecoilRoot} from 'recoil';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {homeMenuSetHome} from './src/utils/atom';
-import HomeIcon from './assets/svg/home.svg';
-import SelectedHomeIcon from './assets/svg/selectedHome.svg';
-import ChatIcon from './assets/svg/chat.svg';
-import SelectedChatIcon from './assets/svg/selectedChat.svg';
+import BottomTab from './src/components/bottomTab';
 import './gesture-handler';
-import {trigger} from 'react-native-haptic-feedback';
-
-const options = {
-  enableVibrateFallback: true,
-  ignoreAndroidSystemSettings: false,
-};
-
-const Tab = createBottomTabNavigator();
-
-function ProfileIcon() {
-  return <Profile color="#ffffff" size={8} width={24} height={24} />;
-}
-
-function MyTabs() {
-  const [homeMenuButtonSetHome, setHomeMenuButtonSetHome] =
-    useRecoilState(homeMenuSetHome);
-
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: colors.background.black,
-          borderTopColor: colors.background.grey,
-          borderTopWidth: 1,
-        },
-        tabBarActiveTintColor: colors.font.white,
-        tabBarInactiveTintColor: colors.font.grey,
-        tabBarIconStyle: {
-          marginTop: 14,
-        },
-        tabBarLabelStyle: {
-          fontSize: 12,
-          fontFamily: 'WantedSans-Medium',
-          marginTop: 6,
-        },
-        tabBarItemStyle: {
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-      }}>
-      <Tab.Screen
-        name={homeMenuButtonSetHome ? 'Home' : 'Live'}
-        component={homeMenuButtonSetHome ? Home : Live}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({focused}) =>
-            focused ? (
-              <SelectedHomeIcon width={24} height={24} />
-            ) : (
-              <HomeIcon width={24} height={24} />
-            ),
-        }}
-        listeners={({route}) => ({
-          tabPress: () => {
-            trigger('impactLight', options);
-            if (route.name === 'Home') {
-              setHomeMenuButtonSetHome(false);
-            } else if (route.name === 'Live') {
-              setHomeMenuButtonSetHome(true);
-            }
-          },
-        })}
-      />
-
-      <Tab.Screen
-        name="Chat"
-        component={Chat}
-        options={{
-          tabBarLabel: 'Chat',
-          tabBarIcon: ({focused}) =>
-            focused ? (
-              <SelectedChatIcon width={24} height={24} />
-            ) : (
-              <ChatIcon width={24} height={24} />
-            ),
-        }}
-        listeners={() => ({
-          tabPress: () => {
-            trigger('impactLight', options);
-            setHomeMenuButtonSetHome(false);
-          },
-        })}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={Setting}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ProfileIcon,
-        }}
-        listeners={() => ({
-          tabPress: () => {
-            trigger('impactLight', options);
-            setHomeMenuButtonSetHome(false);
-          },
-        })}
-      />
-    </Tab.Navigator>
-  );
-}
 
 function App(): React.JSX.Element {
   initializeKakaoSDK(KAKAO_NATIVE_APP_KEY);
-  // getKeyHashAndroid().then(console.log);
+
   return (
     <RecoilRoot>
       <NavigationContainer>
         <SafeAreaProvider>
           <StatusBar barStyle="default" />
           {/* <Login /> */}
-          <MyTabs />
+          <BottomTab />
         </SafeAreaProvider>
       </NavigationContainer>
     </RecoilRoot>
---
+++ src/components/bottomTab.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import HomeIcon from '../../assets/svg/home.svg';
+import SelectedHomeIcon from '../../assets/svg/selectedHome.svg';
+import ChatIcon from '../../assets/svg/chat.svg';
+import SelectedChatIcon from '../../assets/svg/selectedChat.svg';
+import {trigger} from 'react-native-haptic-feedback';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {useRecoilState} from 'recoil';
+import {homeMenuSetHome} from '../utils/atom';
+import Profile from './profile';
+import colors from '../styles/color';
+import Chat from '../screens/chat/chat';
+import Setting from '../screens/settings/setting';
+import Home from '../screens/home/home';
+import Live from '../screens/home/live';
+
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
+
+const Tab = createBottomTabNavigator();
+
+function ProfileIcon() {
+  return (
+    <Profile color={colors.background.white} size={8} width={24} height={24} />
+  );
+}
+
+export default function BottomTab() {
+  const [homeMenuButtonSetHome, setHomeMenuButtonSetHome] =
+    useRecoilState(homeMenuSetHome);
+
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: colors.background.black,
+          borderTopColor: colors.background.grey,
+          borderTopWidth: 1,
+        },
+        tabBarActiveTintColor: colors.font.white,
+        tabBarInactiveTintColor: colors.font.grey,
+        tabBarIconStyle: {
+          marginTop: 14,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontFamily: 'WantedSans-Medium',
+          marginTop: 6,
+        },
+        tabBarItemStyle: {
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      }}>
+      <Tab.Screen
+        name={homeMenuButtonSetHome ? 'Home' : 'Live'}
+        component={homeMenuButtonSetHome ? Home : Live}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({focused}) =>
+            focused ? (
+              <SelectedHomeIcon width={24} height={24} />
+            ) : (
+              <HomeIcon width={24} height={24} />
+            ),
+        }}
+        listeners={({route}) => ({
+          tabPress: () => {
+            trigger('impactLight', options);
+            if (route.name === 'Home') {
+              setHomeMenuButtonSetHome(false);
+            } else if (route.name === 'Live') {
+              setHomeMenuButtonSetHome(true);
+            }
+          },
+        })}
+      />
+
+      <Tab.Screen
+        name="Chat"
+        component={Chat}
+        options={{
+          tabBarLabel: 'Chat',
+          tabBarIcon: ({focused}) =>
+            focused ? (
+              <SelectedChatIcon width={24} height={24} />
+            ) : (
+              <ChatIcon width={24} height={24} />
+            ),
+        }}
+        listeners={() => ({
+          tabPress: () => {
+            trigger('impactLight', options);
+            setHomeMenuButtonSetHome(false);
+          },
+        })}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Setting}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ProfileIcon,
+        }}
+        listeners={() => ({
+          tabPress: () => {
+            trigger('impactLight', options);
+            setHomeMenuButtonSetHome(false);
+          },
+        })}
+      />
+    </Tab.Navigator>
+  );
+}
