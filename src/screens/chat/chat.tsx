ed5bff8f480a5df165d8a887b6086890f21f5747
SID12g <sid12g@naver.com>
2024-11-21 22:09:35 +0900

    fix: chat KeyboardAvoidingView, max height

--- src/screens/chat/chat.tsx
+++ src/screens/chat/chat.tsx
@@ -1,6 +1,8 @@
 import React, {useRef} from 'react';
 import {css} from '@emotion/native';
 import {
+  KeyboardAvoidingView,
+  Platform,
   ScrollView,
   Text,
   TextInput,
@@ -22,148 +24,156 @@ export default function Chat() {
   const scrollViewRef = useRef<ScrollView>(null);
 
   return (
-    <View
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={css`
-        width: 100%;
-        height: 100%;
+        flex: 1;
         background-color: ${colors.background.black};
       `}>
       <View
         style={css`
-          height: ${`${insets.top}px`};
-        `}
-      />
-      <View
-        style={css`
           width: 100%;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          border-bottom-width: 1px;
-          border-color: ${colors.background.grey};
-          padding-top: 10px;
-          padding-bottom: 10px;
-        `}>
-        <TouchableOpacity
-          style={css`
-            margin-right: 8px;
-            margin-left: 20px;
-          `}
-          onPress={() => navigation.goBack()}>
-          <Arrow />
-        </TouchableOpacity>
-        <Profile
-          width={32}
-          height={32}
-          size={12}
-          color={colors.background.yellow}
-        />
-        <Text
-          style={css`
-            color: ${colors.font.white};
-            font-family: 'WantedSans-Medium';
-            font-size: 18px;
-            margin-left: 8px;
-          `}>
-          Incredible Pickle
-        </Text>
-      </View>
-      <ScrollView
-        style={css`
-          padding-left: 20px;
-          padding-right: 20px;
-        `}
-        contentContainerStyle={css`
-          flex-grow: 1;
-          justify-content: flex-end;
-        `}
-        showsVerticalScrollIndicator={false}
-        ref={scrollViewRef}
-        onContentSizeChange={() => {
-          scrollViewRef.current?.scrollToEnd({animated: false});
-        }}
-        onLayout={() => {
-          scrollViewRef.current?.scrollToEnd({animated: false});
-        }}>
-        <Text
-          style={css`
-            color: ${colors.font.grey};
-            font-family: 'WantedSans-Medium';
-            font-size: 14px;
-            text-align: center;
-            margin-top: 20px;
-            margin-bottom: 24px;
-          `}>
-          August 15th (Mon) 2024
-        </Text>
-        <MyMessage content="How was the pickle chat?" />
-        <OtherMessage content="How was the pickle chat?" />
-        <MyMessage content="How was the pickle chat?" />
-        <OtherMessage content="How was the pickle chat?" />
-        <MyMessage content="How was the pickle chat?" />
-        <OtherMessage content="How was the pickle chat?" />
-        <MyMessage content="How was the pickle chat?" />
-        <OtherMessage content="How was the pickle chat?" />
-        <MyMessage content="How was the pickle chat?" />
-        <OtherMessage content="How was the pickle chat?" />
-        <MyMessage content="How was the pickle chat?" />
-        <OtherMessage content="How was the pickle chat?" />
-        <View
-          style={css`
-            height: ${`${insets.top + 40}px`};
-          `}
-        />
-      </ScrollView>
-      <View
-        style={css`
-          display: flex;
-          flex-direction: row;
-          align-items: center;
+          height: 100%;
+          background-color: ${colors.background.black};
         `}>
         <View
           style={css`
-            background-color: ${colors.background.black};
-            position: absolute;
-            bottom: 0px;
-            left: 20px;
-            right: 20px;
+            height: ${`${insets.top}px`};
+          `}
+        />
+        <View
+          style={css`
+            width: 100%;
             display: flex;
             flex-direction: row;
             align-items: center;
-            padding-top: 8px;
-            padding-bottom: 40px;
+            border-bottom-width: 1px;
+            border-color: ${colors.background.grey};
+            padding-top: 10px;
+            padding-bottom: 10px;
           `}>
-          <TextInput
-            style={css`
-              background-color: ${colors.background.grey};
-              font-family: 'WantedSans-Medium';
-              font-size: 16px;
-              color: ${colors.font.grey};
-              padding: 14px 20px;
-              padding-right: 60px;
-              border-radius: 24px;
-              margin-right: 8px;
-              flex: 1;
-            `}
-            placeholder={'Type a message'}
-            placeholderTextColor={colors.font.grey}
-            onChangeText={() => {}}
-            multiline={true}
-          />
           <TouchableOpacity
             style={css`
-              width: 36px;
-              height: 36px;
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              background-color: ${colors.background.white};
-              border-radius: 100px;
-            `}>
-            <Image />
+              margin-right: 8px;
+              margin-left: 20px;
+            `}
+            onPress={() => navigation.goBack()}>
+            <Arrow />
           </TouchableOpacity>
+          <Profile
+            width={32}
+            height={32}
+            size={12}
+            color={colors.background.yellow}
+          />
+          <Text
+            style={css`
+              color: ${colors.font.white};
+              font-family: 'WantedSans-Medium';
+              font-size: 18px;
+              margin-left: 8px;
+            `}>
+            Incredible Pickle
+          </Text>
+        </View>
+        <ScrollView
+          style={css`
+            padding-left: 20px;
+            padding-right: 20px;
+          `}
+          contentContainerStyle={css`
+            flex-grow: 1;
+            justify-content: flex-end;
+          `}
+          showsVerticalScrollIndicator={false}
+          ref={scrollViewRef}
+          onContentSizeChange={() => {
+            scrollViewRef.current?.scrollToEnd({animated: false});
+          }}
+          onLayout={() => {
+            scrollViewRef.current?.scrollToEnd({animated: false});
+          }}>
+          <Text
+            style={css`
+              color: ${colors.font.grey};
+              font-family: 'WantedSans-Medium';
+              font-size: 14px;
+              text-align: center;
+              margin-top: 20px;
+              margin-bottom: 24px;
+            `}>
+            August 15th (Mon) 2024
+          </Text>
+          <MyMessage content="How was the pickle chat?" />
+          <OtherMessage content="How was the pickle chat?" />
+          <MyMessage content="How was the pickle chat?" />
+          <OtherMessage content="How was the pickle chat?" />
+          <MyMessage content="How was the pickle chat?" />
+          <OtherMessage content="How was the pickle chat?" />
+          <MyMessage content="How was the pickle chat?" />
+          <OtherMessage content="How was the pickle chat?" />
+          <MyMessage content="How was the pickle chat?" />
+          <OtherMessage content="How was the pickle chat?" />
+          <MyMessage content="How was the pickle chat?" />
+          <OtherMessage content="How was the pickle chat?" />
+          <View
+            style={css`
+              height: ${`${insets.top + 40}px`};
+            `}
+          />
+        </ScrollView>
+        <View
+          style={css`
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+          `}>
+          <View
+            style={css`
+              background-color: ${colors.background.black};
+              position: absolute;
+              bottom: 0px;
+              left: 20px;
+              right: 20px;
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              padding-top: 8px;
+              padding-bottom: 40px;
+            `}>
+            <TextInput
+              style={css`
+                background-color: ${colors.background.grey};
+                font-family: 'WantedSans-Medium';
+                font-size: 16px;
+                color: ${colors.font.grey};
+                padding: 14px 20px;
+                padding-right: 60px;
+                border-radius: 24px;
+                margin-right: 8px;
+                flex: 1;
+                max-height: 200px;
+              `}
+              placeholder={'Type a message'}
+              placeholderTextColor={colors.font.grey}
+              onChangeText={() => {}}
+              multiline={true}
+            />
+            <TouchableOpacity
+              style={css`
+                width: 36px;
+                height: 36px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-color: ${colors.background.white};
+                border-radius: 100px;
+              `}>
+              <Image />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
